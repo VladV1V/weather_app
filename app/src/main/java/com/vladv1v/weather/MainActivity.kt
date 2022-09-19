a35831b38aaf670980938ea5f4d234aa43cfe38f
VladV1V <mahno444@gmail.com>
2022-09-19 12:24:09 -0600

    Initial commit

---
+++ app/src/main/java/com/vladv1v/weather/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.vladv1v.weather
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.vladv1v.weather.ui.theme.WeatherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherTheme {
+
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WeatherTheme {
+
+    }
+}
